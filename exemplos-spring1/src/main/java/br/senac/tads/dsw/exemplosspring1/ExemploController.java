2cc5dd9465f07dd581bbd55c7d5b499530ac6f8e
Fernando Tsuda <ftsuda.senac@gmail.com>
2019-09-23 20:17:57 -0300

    Exemplos GET

--- exemplos-spring1/src/main/java/br/senac/tads/dsw/exemplosspring1/ExemploController.java
+++ exemplos-spring1/src/main/java/br/senac/tads/dsw/exemplosspring1/ExemploController.java
@@ -3,10 +3,13 @@ package br.senac.tads.dsw.exemplosspring1;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -57,5 +60,42 @@ public class ExemploController {
 		return resposta;
 	}
 	
+	@GetMapping("/ex5/{apelido}")
+	public ModelAndView exemplo5(
+			@PathVariable("apelido") String apelido,
+			@RequestParam(value = "genero", required = false, defaultValue = "0") int genero, 
+			@RequestParam(value = "dtNasc", required = false) String dtNascimento) {
+		ModelAndView resposta = new ModelAndView("exemplo5");
+		resposta.addObject("apelidoParam", apelido);
+		resposta.addObject("generoParam", genero);
+		resposta.addObject("dtNascParam", dtNascimento);
+		return resposta;
+	}
 
+	@GetMapping("/ex6/{apelido}/{numeroSorte}")
+	public ModelAndView exemplo6(
+			@PathVariable("apelido") String apelido,
+			@PathVariable("numeroSorte") int numeroSorte) {
+		ModelAndView resposta = new ModelAndView("exemplo6");
+		resposta.addObject("apelidoParam", apelido);
+		resposta.addObject("numeroSorteParam", numeroSorte);
+		return resposta;
+	}
+
+	@GetMapping("/ex7")
+	public ModelAndView exemplo7(@RequestHeader("user-agent") String userAgent) {
+		ModelAndView resposta = new ModelAndView("exemplo7");
+		boolean mobile = userAgent.toLowerCase().contains("mobile");
+		resposta.addObject("ua", userAgent);
+		resposta.addObject("mobile", mobile);
+		return resposta;
+	}
+	
+	@GetMapping("/ex8")
+	public ModelAndView exemplo8(@RequestHeader Map<String, String> headers) {
+		ModelAndView resposta = new ModelAndView("exemplo8");
+		resposta.addObject("headers", headers);
+		return resposta;
+	}
+	
 }
